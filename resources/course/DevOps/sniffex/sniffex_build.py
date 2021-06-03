7f96d8271a5bea7b0993d82b93d19a5eb00a4b1c
annemfoley <31217196+annemfoley@users.noreply.github.com>
2021-06-03 12:19:04 -0500

    correct logging of sniffex debugging status

--- resources/course/DevOps/sniffex/sniffex_build.py
+++ resources/course/DevOps/sniffex/sniffex_build.py
@@ -17,14 +17,13 @@ if args.debug:
 else:
     os.system("gcc -Wall -o sniffex sniffex.c -lpcap")
 
-
 with open('log.csv', mode='a+') as log_file:
     
     log_writer = csv.writer(log_file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    if not os.path.exists('log.csv'):
-        fieldnamesList = ["datetime", "ipAddress", "domain", "architecture", "platform", "os", "versionNumber", "payload", "activate", "interface", "key", "size", "dateDelay", "timeDelay", "trigger", "persistence", "bang", "downloadURL", "loadShellcode", "reverseShell", "reverseIP", "reversePort", "Notes", "debug", "outputName", "strip", "static"]
+    if os.stat("log.csv").st_size == 0:
+        fieldnamesList = ["datetime", "debug"]
         log_writer.writerow(fieldnamesList)
 
-    log_writer.writerow([str(datetime.datetime.now()), str(args.ipAddress), str(args.domain), str(args.architecture), str(args.platform), str(args.os), str(args.versionNumber), str(args.payload), str(args.activate), str(args.interface), str(args.key), str(args.size), str(args.dateDelay), str(args.timeDelay), str(args.trigger), str(args.persistence), str(args.bang), str(args.downloadURL), str(args.loadShellcode), str(args.reverseShell), str(args.reverseIP), str(args.reversePort), str(args.notes), str(args.debug), str(args.outputName), str(args.strip), str(args.static)])
+    log_writer.writerow([str(datetime.datetime.now()), str(args.debug)])
 
 

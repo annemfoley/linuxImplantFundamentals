88a649b5357904d928f1d926e13886bb506ce615
annemfoley <31217196+annemfoley@users.noreply.github.com>
2021-06-03 14:46:21 -0500

    added comments to python build script

--- resources/course/DevOps/sniffex/sniffex_build.py
+++ resources/course/DevOps/sniffex/sniffex_build.py
@@ -3,26 +3,32 @@ import datetime
 import os
 import csv
 
+# initialize the parser
 parser = argparse.ArgumentParser(prog = 'sniffex_build', 
                                 usage = '%(prog)s [options]',
                                 description = 'compile the sniffex program')
 
+# define all arguments
 parser.add_argument("-d", "--debug", action="store_true",
                     help="compile with debugging")
 
 args = parser.parse_args()
 
+# compile with or without debugging based on the -d argument
 if args.debug:
-    exit_code = os.system("gcc -DDEBUG -Wall -o sniffexDebug sniffex.c -lpcap 2> error.txt")
+    exit_code = os.system("gcc -DDEBUG -Wall -o sniffexDebug sniffex.c -lpcap 2> compile_error.txt")
 else:
-    exit_code = os.system("gcc -Wall -o sniffex sniffex.c -lpcap 2> error.txt")
+    exit_code = os.system("gcc -Wall -o sniffex sniffex.c -lpcap 2> compile_error.txt")
 
-error_file = open('error.txt',mode='r')
+# if there are errors with compiling, they will be stored in error.txt
+error_file = open('compile_error.txt',mode='r')
 error = error_file.read()
 error_file.close()
 
-os.remove('error.txt')
+# we are done with this temporary file
+os.remove('compile_error.txt')
 
+# log the time, any errors with compilation, and the arguments used to compile
 with open('log.csv', mode='a+') as log_file:
     
     log_writer = csv.writer(log_file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
